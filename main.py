c3fe8762480063749c10889f3a52cb7c8f6c18f4
Daniil <dkuznetsov17@gmail.com>
2022-12-03 17:27:42 +0000

    I love accidently deleting projects

--- main.py
+++ main.py
@@ -5,164 +5,125 @@ from torch import nn
 from torch import optim
 from typing import Any
 
-
-def monte_carlo_reward(rewards: t.Tensor, crit: float, gamma: float = 0.99) -> t.Tensor:
-    returns = t.zeros_like(rewards)
-    for i in reversed(range(len(rewards))):
-        crit = rewards[i].item() + gamma * crit
-        returns[i] = crit
-    return returns
-
+LEARNING_RATE = 3e-5 
+EPISODES = 1000
+STEPS = 500
+ENTROPY = 0.001
 
 class ActorCritic(nn.Module):
-    def __init__(self, inp_dim : int, out_dim : int) -> None:
+    def __init__(self, inp_dim, hidden_dim, out_dim):
         super().__init__()
         self.inp_dim = inp_dim
+        self.hidden_dim = hidden_dim
         self.out_dim = out_dim
 
         # Shared
-        self.linear1 = nn.Linear(inp_dim, 256)
-        self.linear2 = nn.Linear(256, 256)
+        self.linear1 = nn.Linear(inp_dim, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
 
         # Divergence 
-        self.actor = nn.Linear(256, out_dim)
-        self.critic = nn.Linear(256, 1)
+        self.actor_mean = nn.Linear(hidden_dim, out_dim)
+        self.actor_std = nn.Linear(hidden_dim, out_dim)
+
+        self.critic = nn.Linear(hidden_dim, 1)
+
+    def gauss(self, z):
+        
+        mean, std = t.tanh(self.actor_mean(z)), t.nn.functional.softplus(self.actor_std(z))
+        
+        dist = t.distributions.normal.Normal(t.zeros(self.out_dim), t.ones(self.out_dim))
+
+        action = (mean + std * dist.sample()).detach()
+
+        action_logProb = -((action - mean) ** 2) / (2 * std ** 2) - t.log(std) - t.log(t.sqrt(t.tensor([2 * t.pi])))
+
+        return action, action_logProb, mean, std
 
 
     def forward(self, x : t.Tensor) -> tuple[t.Tensor, t.Tensor]:        
-        self.z = nn.functional.relu(self.linear1(x))
-        self.z = nn.functional.relu(self.linear2(self.z))
+        z = nn.functional.relu(self.linear1(x))
+        z = nn.functional.relu(self.linear2(z))
         
-        return nn.functional.log_softmax(self.actor(self.z), dim=0), self.critic(self.z)
+        action, action_logProb, mean, std = self.gauss(z)
+
+        return action, action_logProb, mean, std, self.critic(z)
 
 
-def a2c_train_loop(mdl : nn.Module, env : Any, episodes : int, max_steps : int = 1000) -> None:
+def a2c_train_loop(mdl, env):
+    
+    def mcr(rewards: t.Tensor, gamma = 0.99):
+        returns = t.zeros_like(rewards)
+        
+        ret = 0
+        for i in reversed(range(len(rewards))):
+            returns[i] = ret = rewards[i].item() + gamma * ret
+        
+        returns = (returns - t.mean(returns)) / t.std(returns)
 
-    actor_loss_func = lambda action_logProbs, returns, crits : -t.mean(action_logProbs * (returns - crits))
-    critic_loss_func = nn.MSELoss("mean")
+        return returns
 
-    opti = optim.Adam(mdl.parameters(), lr=0.00001)
+
+    opti = optim.Adam(mdl.parameters(), lr=LEARNING_RATE)
    
-    for episode in range(episodes):
+    for episode in range(1, EPISODES + 1):
             
-        state_t, info = env.reset(seed=0)
-        state_t = t.tensor(list(env.decode(state_t))).float()
+        state_t = t.from_numpy(env.reset()[0]).float()
         
-        step = 0
-        
-        rewards = t.zeros(max_steps)
-        crits = t.zeros(max_steps) # Must be here to reset graph
-        action_logProbs = t.empty(max_steps)
-        
-        for _ in range(max_steps):
-            in_taxi = state_t[2] == 4
+        rewards = []
+        crits = []
+        action_logProbs = []
+        stds = []
 
-            state_t[0] /= 5
-            state_t[1] /= 5
-            state_t[2] /= 5
-            state_t[3] /= 4
+        for step in range(1, STEPS + 1):
             
-            action_logProb, crit_t = mdl(state_t)
+            action, action_logProb, mean, std, crit_t = mdl(state_t)
             
-            # Explore epsilon
-            if t.rand(1).item() < min(0.90, episode / (episodes / 2)):
-                action = t.distributions.categorical.Categorical(logits=action_logProb).sample()
-            else:
-                action = t.distributions.categorical.Categorical(probs=t.ones(1, mdl.out_dim)).sample()
+            state_tt, reward_tt, terminated, truncated, info = env.step(t.clip(action, -1, 1).numpy())
             
-            state_tt, reward_tt, terminated, _, info = env.step(action.item())
-            
-            state_tt = t.tensor(list(env.decode(state_tt))).float()
-            
-            if (t.equal(state_tt, state_t)): 
-                reward_tt = -5
-            
-            if (not in_taxi and state_tt[2] == 4):
-                reward_tt = 100
-           
-            reward_tt /= 100
+            crits.append(crit_t)
+            rewards.append(reward_tt)
+            action_logProbs.append(action_logProb)
+            stds.append(std)
 
-            crits[step] = crit_t
-            rewards[step] = reward_tt
-            action_logProbs[step] = action_logProb[action]
-
-            step += 1 
-            state_t = state_tt
-
-            if terminated:
+            state_t = t.from_numpy(state_tt).float()
+            
+            if terminated or truncated:
                 break
+             
+        rewards = t.tensor(rewards).unsqueeze(1)
+        crits = t.stack(crits)
+        action_logProbs = t.stack(action_logProbs)
+        stds = t.stack(stds)
             
-            if (not in_taxi and state_tt[2] == 4):
-                break
+        
 
-        _, crit_t = mdl(state_t)
+        returns = mcr(rewards)
+        advantage = returns - crits
 
-        returns = monte_carlo_reward(rewards[:step], crit_t.item())
-        actor_loss = actor_loss_func(action_logProbs[:step], returns, crits[:step].detach());    
-        critic_loss = critic_loss_func(returns, crits[:step])
-        a2c_loss = critic_loss + actor_loss
+        actor_loss = t.sum(-action_logProbs * advantage.detach()) 
+        critic_loss = t.sum(advantage ** 2)
+        entropy_loss = t.sum(-ENTROPY * ((0.5 * t.log(2 * t.pi * stds ** 2)) + 0.5))
+
+        a2c_loss = critic_loss + actor_loss + entropy_loss
        
         opti.zero_grad()
-        
         a2c_loss.backward()
-        
-        if (episode % 10 == 0):
-            param_sum = 0
-            param_cnt = 0
-            for param in mdl.parameters():
-                param_sum += t.sum(param.data)
-                param_cnt += t.prod(t.tensor([param.shape]))
-            
-            print(env.render())
-            print(f"EPISODE: {episode}, STEPS: {step}, {[x.item() for x in action_logProb]}, CRIT LOSS: {critic_loss.item():.4f}, ACT LOSS: {actor_loss.item():.4f}, PARAM AVG: {param_sum / param_cnt}")
-
         opti.step()
-
-def play_game(mdl, env):
-    
-    while(True):
-        step = 0
         
-        state_t, info = env.reset(seed=0)
-        #state_t = nn.functional.one_hot(t.tensor([state_t]), num_classes=env.observation_space.n)[0].float()
-        state_t = t.tensor(list(env.decode(state_t))).float()
+        if (episode % 1 == 0):
+            print(f"EPISODE: {episode}, STEPS: {step}\n{action.detach().numpy()}\nCRIT LOSS: {critic_loss.item():.4f}, ACT LOSS: {actor_loss.item():.4f}, ENTROPY LOSS: {entropy_loss:.4f}\nMAX MEAN: {mean[t.argmax(t.abs(mean))]}, MAX STD: {t.max(std)}\n")
 
-
-        while(True):
-            state_t[0] /= 5
-            state_t[1] /= 5
-            state_t[2] /= 5
-            state_t[3] /= 4
-            
-            action_logProb, _ = mdl(state_t) 
-
-            action = t.distributions.categorical.Categorical(logits=action_logProb).sample()
-
-            
-            state_t, reward_tt, terminated, _, _ = env.step(action.item())
-            state_t = t.tensor(list(env.decode(state_t))).float()
-
-
-            step += 1
-            print(step, action_logProb, t.argmax(action_logProb))
-
-            if terminated:
-                break
 
 
 def main() -> None:
-    environment = gym.make("Taxi-v3", render_mode="ansi")
+    environment = gym.make("Ant-v4")
     
-    model = ActorCritic(4, environment.action_space.n)
+    model = ActorCritic(environment.observation_space.shape[0], 256, environment.action_space.shape[0])
     
-    a2c_train_loop(model, environment, 2000, 500)
+    a2c_train_loop(model, environment)
     t.save(model, "model.torch")
 
     #model = t.load("model.torch")
-
-    environment = gym.make("Taxi-v3", render_mode="human")
-
-    play_game(model, environment)
 
     environment.close()
 
