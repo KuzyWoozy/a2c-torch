161aa6cdf7a702e46790b171531bdfe6dbff462e
Daniil <dkuznetsov17@gmail.com>
2022-12-10 13:22:21 +0000

    initial GPU porting

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+import time
 import gym
 import numpy as np
 import torch as t
 from torch import nn
 from torch import optim
-from typing import Any
+
+
+t.backends.cuda.matmul.allow_tf32 = True
+t.set_default_dtype(t.float32)
 
 LEARNING_RATE = 3e-3 
 EPISODES = 10000
 STEPS = 500
 ENTROPY = 0.0001
+
+CPU = t.device("cpu")
+GPU = t.device("cuda" if t.cuda.is_available() else "cpu")
 
 
 class ActorCritic(nn.Module):
@@ -51,7 +58,7 @@ class ActorCritic(nn.Module):
 def a2c_train_loop(mdl, env):
     
     def mcr(rewards: t.Tensor, gamma = 0.99):
-        returns = t.zeros_like(rewards)
+        returns = t.zeros_like(rewards, device=GPU)
         
         ret = 0
         for i in reversed(range(len(rewards))):
@@ -64,52 +71,55 @@ def a2c_train_loop(mdl, env):
 
     opti = optim.Adam(mdl.parameters(), lr=LEARNING_RATE)
    
-    for episode in range(1, EPISODES + 1):
+    for episode in range(0, EPISODES):
             
         state_t = t.from_numpy(env.reset()[0]).float()
-        
-        rewards = []
-        crits = []
-        action_logProbs = []
-        stds = []
+        state_t.to(GPU)
 
-        for step in range(1, STEPS + 1):
-            
-            action, action_logProb, mean, std, crit_t = mdl(state_t)
-            
-            state_tt, reward_tt, terminated, truncated, info = env.step(action.detach().numpy())
-            
-            crits.append(crit_t)
-            rewards.append(reward_tt)
-            action_logProbs.append(action_logProb)
-            stds.append(std)
+        # Reset graph, ensure correct broadcasting 
+        rewards = t.zeros(STEPS, 1, device=GPU)
+        crits = t.zeros(STEPS, 1, device=GPU)
+        action_logProbs = t.zeros(STEPS, mdl.out_dim, device=GPU)
+        stds = t.zeros(STEPS, mdl.out_dim, device=GPU)
 
-            state_t = t.from_numpy(state_tt).float()
+        for step in range(0, STEPS):
             
+            action, action_logProb, mean, std, crit_t = mdl(state_t)    
+            action.to(CPU)
+
+            state_tt, reward_tt, terminated, truncated, info = env.step(action.numpy())
+            
+            crits[step] = crit_t
+            rewards[step] = reward_tt
+            action_logProbs[step] = action_logProb
+            stds[step] = std
+
             if terminated:
                 break
              
-        rewards = t.tensor(rewards).unsqueeze(1)
-        crits = t.stack(crits)
-        action_logProbs = t.stack(action_logProbs)
-        stds = t.stack(stds)
-            
+            state_t = t.from_numpy(state_tt).float().to(GPU)
         
+        rewards = rewards[:step + 1] 
+        crits = crits[:step + 1]
+        action_logProbs = action_logProbs[:step + 1] 
+        stds = stds[:step + 1]
+            
 
         returns = mcr(rewards)
         advantage = returns - crits
-
         actor_loss = t.sum(-action_logProbs * advantage.detach()) 
         critic_loss = t.sum(advantage ** 2)
         entropy_loss = t.sum(-ENTROPY * ((0.5 * t.log(2 * t.pi * stds ** 2)) + 0.5))
 
         a2c_loss = critic_loss + actor_loss + entropy_loss
-       
+      
+        print(a2c_loss.device)
+
         opti.zero_grad()
         a2c_loss.backward()
         opti.step()
         
-        if (episode % 1 == 0):
+        if (episode % 10 == 0):
             print(f"EPISODE: {episode}, STEPS: {step}\n{action.detach().numpy()}\nCRIT LOSS: {critic_loss.item():.4f}, ACT LOSS: {actor_loss.item():.4f}, ENTROPY LOSS: {entropy_loss:.4f}\nMEAN MAX: {mean[t.argmax(t.abs(mean))]}, MEAN AVG: {t.mean(mean)}, STD MAX: {t.max(std)}, STD AVG: {t.mean(std)}\n", flush=True) 
 
 
@@ -118,10 +128,11 @@ def main() -> None:
     environment = gym.make("Ant-v4")
     
     model = ActorCritic(environment.observation_space.shape[0], 100, environment.action_space.shape[0])
+    model.to(GPU)
     
     a2c_train_loop(model, environment)
     
-    t.save(model, "model.torch")
+    t.save(model, f"model-{round(time.time())}.torch")
     #model = t.load("model.torch")
 
     environment.close()
